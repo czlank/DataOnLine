4c8d30f8f4f489b1336e9fdf98b09d0957cc85af
czlank <41607842@qq.com>
2017-12-25 22:32:54 +0800

    完成数据的解包、再封装

--- src/com/dataonline/action/ValueQuery.java
+++ src/com/dataonline/action/ValueQuery.java
@@ -9,13 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
 import net.sf.json.util.JSONStringer;
 
 import org.apache.log4j.Logger;
 
 import com.dataonline.factory.MaintenanceFactory;
+import com.dataonline.intfc.NodeOpt;
+import com.dataonline.intfc.TypeOpt;
 import com.dataonline.intfc.UserOpt;
 import com.dataonline.intfc.ValueOpt;
+import com.dataonline.pojo.Node;
+import com.dataonline.pojo.Type;
 import com.dataonline.pojo.User;
 import com.dataonline.pojo.Value;
 import com.dataonline.util.LineNo;
@@ -66,9 +73,96 @@ public class ValueQuery extends HttpServlet {
         	return;
         }
         
-        response.getWriter().println(getFormatResult("ok", "" + vecValue.get(0).getValue()));
+        String jsonValue = RePackage(vecUser.get(0).getID(), vecValue.get(0).getValue());
+        response.getWriter().println(getFormatResult("ok", "" + jsonValue));
 	}
-    
+	
+	private String RePackage(int userId, String value) {
+		try {
+			JSONObject json = JSONObject.fromObject(value);
+		
+			// 解析type
+			JSONArray jsonTypeArray = JSONArray.fromObject(json.get("type"));
+			
+			// 重新组装数据
+			JSONStringer stringerTypeArray = new JSONStringer();
+			stringerTypeArray.array();
+			
+			for(int i = 0; i < jsonTypeArray.size(); i++) {
+				JSONObject jsonType = JSONObject.fromObject(jsonTypeArray.getString(i));
+				
+				// 解析类型id
+        		int typeId = Integer.parseInt(String.valueOf(jsonType.get("t")));
+        		
+        		// 查询type表，获取类型名称、阈值
+        		Type type = new Type();
+        		type.setType(typeId);
+        		type.setOpt(TypeOpt.O_TYPE.get());
+        		
+        		Vector<Type> vecType = MaintenanceFactory.getInstance().getMaintenance().typeQuery(type);
+        		String typeName = "未知类型(" + typeId + ")";
+        		double min = 0.0, max = 0.0;
+        		if (vecType != null && 1 == vecType.size()) {
+        			typeName = vecType.get(0).getName();
+        			min = vecType.get(0).getMin();
+        			max = vecType.get(0).getMax();
+        		}
+        		
+        		// 解析此类型的数据
+        		JSONArray jsonValueArray = JSONArray.fromObject(jsonType.get("v"));
+        		
+        		// 重新组装数据
+        		JSONStringer stringerValueArray = new JSONStringer();
+        		stringerValueArray.array();
+        		
+        		for (int j = 0; j < jsonValueArray.size(); j++) {
+        			// 解析节点数据
+        			JSONObject jsonValue = JSONObject.fromObject(jsonValueArray.getString(j));
+        			
+        			int nodeId = Integer.parseInt(String.valueOf(jsonValue.get("n")));
+        			
+        			// 查询node表，获取节点的别名
+        			Node node = new Node();
+        			node.setValue(nodeId);
+        			node.setOpt(NodeOpt.O_VALUE.get());
+        			
+        			Vector<Node> vecNode = MaintenanceFactory.getInstance().getMaintenance().nodeQuery(userId, node);
+        			String nodeAlias = new String("未命名节点");
+        			if (vecNode != null && 1 == vecNode.size()) {
+        				nodeAlias = vecNode.get(0).getName();
+        			}
+        			
+        			stringerValueArray.object()
+        				.key("n").value(jsonValue.get("n"))		// 节点Id
+        				.key("a").value(nodeAlias)				// 节点别名
+        				.key("v").value(jsonValue.get("v"))		// 节点值
+        				.endObject();
+        		}
+        		
+        		stringerValueArray.endArray();
+        		
+        		stringerTypeArray.object()
+        			.key("t").value(typeName)						// 类型名称
+        			.key("min").value(min)							// 最小值
+        			.key("max").value(max)							// 最大值
+        			.key("v").value(stringerValueArray.toString())	// 该类型中的数据
+        			.endObject();
+			}
+			
+			stringerTypeArray.endArray();
+			
+			JSONStringer stringer = new JSONStringer();
+			stringer.object().key("type").value(stringerTypeArray.toString()).endObject();
+			return stringer.toString();
+		} catch (JSONException e) {
+			log.error(LineNo.getFileName() + ":L" + LineNo.getLineNumber() + " - " + e.getMessage());			
+		} catch (NumberFormatException e) {
+            log.error(LineNo.getFileName() + ":L" + LineNo.getLineNumber() + " - " + e.getMessage());
+        }
+		
+		return "";
+	}
+
     private String getFormatResult(String result, String tipMsg) {
         JSONStringer stringer = new JSONStringer();
         
--- src/com/dataonline/tag/user/UserCollect.java
+++ src/com/dataonline/tag/user/UserCollect.java
@@ -7,9 +7,7 @@ import javax.servlet.jsp.tagext.TagSupport;
 
 import org.apache.log4j.Logger;
 
-import net.sf.json.JSONArray;
 import net.sf.json.JSONException;
-import net.sf.json.JSONObject;
 import net.sf.json.util.JSONStringer;
 
 import com.dataonline.factory.MaintenanceFactory;
@@ -24,7 +22,6 @@ public class UserCollect extends TagSupport {
 	
 	@Override
 	public int doStartTag() throws JspException {
-		test();
 		try {
             User user = new User();
           
@@ -77,68 +74,5 @@ public class UserCollect extends TagSupport {
     	}
     	
         return str.replaceAll("(\r\n|\r|\n|\n\r)", "<br>").replaceAll("\"", "&quot;");
-    }
-    
-    private void test() {
-    	// 构造数据
-    	String nodeValue[] = {value2string(1, 11.1), value2string(2, 22.2)};
-    	String nodeValueArray = array2string(nodeValue, 2);
-    	String typeValue[] = {type2string(1, nodeValueArray), type2string(2, nodeValueArray)};
-    	String typeValueArray = array2string(typeValue, 2);
-    	
-    	String sTotalString = "{\"type\":" + typeValueArray + "}";
-    	
-    	// 解析数据
-    	try {
-    		JSONObject json = JSONObject.fromObject(sTotalString);
-
-    		// 解析type
-        	JSONArray jsonTypeArray = JSONArray.fromObject(json.get("type"));
-        	System.out.println(jsonTypeArray.size());
-        	if(jsonTypeArray.size() > 0) {
-	        	for(int i = 0; i < jsonTypeArray.size(); i++) {
-	        		JSONObject jsonType = JSONObject.fromObject(jsonTypeArray.getString(i));
-	        		
-	        		// 解析类型id
-	        		System.out.println(jsonType.get("t"));
-	        		// 解析此类型的数据
-	        		JSONArray jsonValueArray = JSONArray.fromObject(jsonType.get("v"));
-	        		
-	        		for (int j = 0; j < jsonValueArray.size(); j++) {
-	        			// 解析节点数据
-	        			JSONObject jsonValue = JSONObject.fromObject(jsonValueArray.getString(j));
-	        			
-	        			// 解析节点Id
-	        			System.out.println(jsonValue.get("n"));
-	        			// 解析节点值
-	        			System.out.println(jsonValue.get("v"));
-	        		}
-	        	}
-        	}
-        } catch (JSONException e) {
-        	System.out.println(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    private String value2string(int nodeId, double value) {
-    	return "{\"n\":" + nodeId + ",\"v\":" + value + "}";
-    }
-    
-    private String type2string(int typeId, String value) {
-    	return "{\"t\":" + typeId + ",\"v\":" + value + "}";
-    }
-    
-    private String array2string(String array[], int size) {
-    	String v = "[";
-    	
-    	for (int i = 0; i < size - 1; i++) {
-    		v += array[i] + ",";
-    	}
-    	
-    	v += array[size - 1] + "]";
-    	
-    	return v;
     }
 }
